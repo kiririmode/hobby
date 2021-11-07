1b253a7b6d29a95d54c75dbaf2dc3b2205d9a757
kiririmode <kiririmode@gmail.com>
2021-11-07 17:25:09 +0900

    feat: Terraform Backend

---
+++ aws/backend/main.tf
@@ -0,0 +1,38 @@
+provider "aws" {}
+
+resource "aws_s3_bucket" "this" {
+  bucket        = "kiririmode-tfbackend"
+  acl           = "private"
+  force_destroy = false
+
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  object_lock_configuration {
+    object_lock_enabled = "Enabled"
+  }
+  tags = {
+    Name = "S3 Remote Terraform State Store"
+  }
+}
+
+resource "aws_dynamodb_table" "this" {
+  name           = "terraform_state"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  tags = {
+    "Name" = "DynamoDB Terraform State Lock Table"
+  }
+}
