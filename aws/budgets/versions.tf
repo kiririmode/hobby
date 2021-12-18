567c6b78d8b888c9c867039bae991d187ec2f4b3
kiririmode <kiririmode@gmail.com>
2021-12-19 07:12:18 +0900

    fix: Terraformを1.0.12に、AWS Providerを3.70.0に最新化

--- aws/budgets/versions.tf
+++ aws/budgets/versions.tf
@@ -2,16 +2,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.64"
+      version = "3.70.0"
     }
   }
-  required_version = "~> 1.0.10"
+  required_version = "1.1.2"
 
   backend "s3" {
     bucket         = "kiririmode-tfbackend"
     key            = "budgets"
     encrypt        = true
     dynamodb_table = "terraform_state"
-    region = "ap-northeast-1"
+    region         = "ap-northeast-1"
   }
 }
